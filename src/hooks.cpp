45ff16669e05b8986e0170891ddcba57dcdf33e5
mat <26722564+matcool@users.noreply.github.com>
2024-10-30 09:04:32 -0300

    rename macros

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -32,19 +32,19 @@ class $modify(CCMouseDispatcher) {
 
 // 2.2 adds some new arguments to the dispatchers
 #if GEODE_COMP_GD_VERSION >= 22000
-	#define NONLEGACY(...) __VA_ARGS__
+	#define IF_2_2(...) __VA_ARGS__
 #else
-	#define NONLEGACY(...)
+	#define IF_2_2(...)
 #endif
 
 class $modify(CCIMEDispatcher) {
-	void dispatchInsertText(const char* text, int len NONLEGACY(, enumKeyCodes keys)) {
+	void dispatchInsertText(const char* text, int len IF_2_2(, enumKeyCodes keys)) {
 		if (!ImGuiCocos::get().isInitialized())
-			return CCIMEDispatcher::dispatchInsertText(text, len NONLEGACY(, keys));
+			return CCIMEDispatcher::dispatchInsertText(text, len IF_2_2(, keys));
 
 		auto& io = ImGui::GetIO();
 		if (!io.WantCaptureKeyboard) {
-			CCIMEDispatcher::dispatchInsertText(text, len NONLEGACY(, keys));
+			CCIMEDispatcher::dispatchInsertText(text, len IF_2_2(, keys));
 		}
 		std::string str(text, len);
 		io.AddInputCharactersUTF8(str.c_str());
@@ -96,9 +96,9 @@ bool shouldBlockInput() {
 }
 
 class $modify(CCKeyboardDispatcher) {
-	bool dispatchKeyboardMSG(enumKeyCodes key, bool down NONLEGACY(, bool idk)) {
+	bool dispatchKeyboardMSG(enumKeyCodes key, bool down IF_2_2(, bool repeat)) {
 		if (!ImGuiCocos::get().isInitialized())
-			return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down NONLEGACY(, idk));
+			return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down IF_2_2(, repeat));
 
 		const bool shouldEatInput = ImGui::GetIO().WantCaptureKeyboard || shouldBlockInput();
 		if (shouldEatInput || !down) {
@@ -110,7 +110,7 @@ class $modify(CCKeyboardDispatcher) {
 		if (shouldEatInput) {
 			return false;
 		} else {
-			return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down NONLEGACY(, idk));
+			return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down IF_2_2(, repeat));
 		}
 	}
 };
