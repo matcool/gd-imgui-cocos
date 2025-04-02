7b22b21d3711694892e863eaccc304ce3dad68e8
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-04-02 18:38:16 -0400

    Update hooks.cpp

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -10,6 +10,7 @@
 
 using namespace geode::prelude;
 
+#ifndef GEODE_IS_IOS
 class $modify(CCMouseDispatcher) {
 	bool dispatchScrollMSG(float y, float x) {
 		if (!ImGuiCocos::get().isInitialized())
@@ -25,6 +26,7 @@ class $modify(CCMouseDispatcher) {
 		return true;
 	}
 };
+#endif
 
 // 2.2 adds some new arguments to the dispatchers
 #if GEODE_COMP_GD_VERSION >= 22000
@@ -97,6 +99,7 @@ bool shouldBlockInput() {
 	return inst.isVisible() && inst.getInputMode() == ImGuiCocos::InputMode::Blocking;
 }
 
+#ifndef GEODE_IS_IOS
 class $modify(CCKeyboardDispatcher) {
 	bool dispatchKeyboardMSG(enumKeyCodes key, bool down IF_2_2(, bool repeat)) {
 		if (!ImGuiCocos::get().isInitialized())
@@ -116,6 +119,7 @@ class $modify(CCKeyboardDispatcher) {
 		}
 	}
 };
+#endif
 
 class $modify(CCTouchDispatcher) {
 	void touches(CCSet* touches, CCEvent* event, unsigned int type) {
