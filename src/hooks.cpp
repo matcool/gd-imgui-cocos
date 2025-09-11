dd4457d963d7ba29d819e106f9d6d88f781b348c
LatterRarity70 <90561697+LatterRarity70@users.noreply.github.com>
2025-09-11 19:27:07 +0400

    Update hooks.cpp

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -131,13 +131,20 @@ class $modify(CCTouchDispatcher) {
 
 		if (!touch) return CCTouchDispatcher::touches(touches, event, type);
 
-		const auto pos = ImGuiCocos::cocosToFrame(touch->getLocation());
-		io.AddMousePosEvent(pos.x, pos.y);
+		// add mouse source events, so imgui can handle touches right -LatterRarity70
+		if (geode::cocos::getMousePos().isZero()) { // no multiple pos event ways (backend.cpp workin on it already)
+			// i mean touch->getLocation() can be different of geode::cocos::getMousePos()! -LatterRarity70
+			const auto pos = ImGuiCocos::cocosToFrame(touch->getLocation());
+			io.AddMouseSourceEvent(ImGuiMouseSource_TouchScreen); // !!!
+			io.AddMousePosEvent(pos.x, pos.y);
+		}
 
 		if (io.WantCaptureMouse || shouldBlockInput()) {
 			if (type == CCTOUCHBEGAN) {
+        		io.AddMouseSourceEvent(ImGuiMouseSource_TouchScreen);
 				io.AddMouseButtonEvent(0, true);
 			} else if (type == CCTOUCHENDED || type == CCTOUCHCANCELLED) {
+        		io.AddMouseSourceEvent(ImGuiMouseSource_TouchScreen);
 				io.AddMouseButtonEvent(0, false);
 			}
 			if (type == CCTOUCHMOVED) {
@@ -145,6 +152,7 @@ class $modify(CCTouchDispatcher) {
 			}
 		} else {
 			if (type != CCTOUCHMOVED) {
+        		io.AddMouseSourceEvent(ImGuiMouseSource_TouchScreen);
 				io.AddMouseButtonEvent(0, false);
 			}
 			CCTouchDispatcher::touches(touches, event, type);
