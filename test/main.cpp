367dcd1180acecb3ae9e46196a841e1b2dbeafd8
mat <26722564+matcool@users.noreply.github.com>
2026-01-20 11:03:12 -0300

    revert 1

--- test/main.cpp
+++ test/main.cpp
@@ -38,11 +38,11 @@ $on_mod(Loaded) {
 #ifndef GEODE_IS_IOS
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 class $modify(ImGuiKeybindHook, cocos2d::CCKeyboardDispatcher) {
-	bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat, double time) {
+	bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
 		if (key == cocos2d::enumKeyCodes::KEY_F4 && isKeyDown) {
 			ImGuiCocos::get().toggle();
 		}
-		return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat, time);
+		return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
 	}
 };
 #endif
